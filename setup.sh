24330b03fcace03787150cdf97873b27f7ccdad8
Werner Boeglin <boeglinw@fiu.edu>
2018-04-16 11:52:58 -0400

    setup.sh includes all data files (hopefully)

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 # setup directory to run simc
-# where simc is located
+# where simc is located, edit this to fit your installation
 SIMCDIR=/data/boeglin.1/HallC/GitHub/deut_simc/
 
 # where the laget directory resides
+# edit this if you need them 
 LAGETDIR=/data/boeglin.1/mceep_3.9/dat/deut_laget
 #
 ln -s $LAGETDIR
@@ -11,9 +12,18 @@ ln -s $SIMCDIR/shms
 ln -s $SIMCDIR/hrsl
 ln -s $SIMCDIR/hrsr
 ln -s $SIMCDIR/python
+ln -s $SIMCDIR/root
+
+# links to physics data files
+# deuteron
+for i in $SIMCDIR/*.dat;do ln -s $i; done
 ln -s $SIMCDIR/h2.theory
 
 
+# link to the executable
+ln -s $SIMCDIR/simc
+
+# default parameter data
 cp $SIMCDIR/nml_default.data .
 
 mkdir ./infiles
@@ -24,11 +34,14 @@ mkdir ./outfiles
 mkdir ./output
 mkdir ./worksim
 
+
 # setup examples in infile
 cp $SIMCDIR/infiles/werner_h2_example.data ./infiles/.
 cp $SIMCDIR/infiles/werner_h2_example_laget.data ./infiles/.
 cp $SIMCDIR/infiles/extra_hydrogen_norad.data ./infiles/.
 cp $SIMCDIR/infiles/extra_hydrogen_rad.data ./infiles/.
+cp $SIMCDIR/infiles/extra_hydrogen_rad.data ./infiles/extra_hydrogen.data
+
 
 
 
